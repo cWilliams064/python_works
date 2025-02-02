a0c44417e77cd83c7ac159bcfcd779893588de72
Courtney Williams <courtneyrwilliams@students.abtech.edu>
2025-02-01 20:08:04 -0500

    Finished  3.3 Your Own List.

--- my-own-list.py
+++ my-own-list.py
@@ -1,3 +1,3 @@
-car_brands = ['Chevorlet', 'Subaru', 'Toyota', 'Honda', 'Cadilac']
+car_brands = ['chevorlet', 'subaru', 'toyota', 'honda', 'cadilac']
 message = f"I would like to buy a {car_brands[0].title()} car."
 print(message)
