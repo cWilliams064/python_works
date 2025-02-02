d5eb3e5a974e440bc1b25420cabf5e952528ad1e
Courtney Williams <courtneyrwilliams@students.abtech.edu>
2025-02-01 22:45:03 -0500

    3.8 My Updated Guest List Completed.

---
+++ guest-list-sorted.py
@@ -0,0 +1,37 @@
+guests = ['elizabeth short', 'ora murray', 'jonbenet ramsey']
+
+invitation = f"{guests[0].title()}, you are invited to a dinner reservation hosted by Courtney Williams."
+print(invitation)
+
+invitation = f"{guests[1].title()}, you are invited to a dinner reservation hosted by Courtney Williams."
+print(invitation)
+
+invitation = f"{guests[2].title()}, you are invited to a dinner reservation hosted by Courtney Williams."
+print(invitation)
+
+print("I found a biger dinner table for more guests!")
+
+guests.insert(0, 'marilyn monroe')
+guests.insert(1, 'joseph zarelli')
+guests.append('maria ridulph')
+
+guests.sort()
+
+
+invitation = f"{guests[0].title()}, you are invited to a dinner reservation hosted by Courtney Williams."
+print(invitation)
+
+invitation = f"{guests[1].title()}, you are invited to a dinner reservation hosted by Courtney Williams."
+print(invitation)
+
+invitation = f"{guests[2].title()}, you are invited to a dinner reservation hosted by Courtney Williams."
+print(invitation)
+
+invitation = f"{guests[3].title()}, you are invited to a dinner reservation hosted by Courtney Williams."
+print(invitation)
+
+invitation = f"{guests[4].title()}, you are invited to a dinner reservation hosted by Courtney Williams."
+print(invitation)
+
+invitation = f"{guests[5].title()}, you are invited to a dinner reservation hosted by Courtney Williams."
+print(invitation)
