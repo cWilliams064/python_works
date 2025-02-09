0740d68ce95c1167e5bc097cb5c9f6304232a6bd
Courtney Williams <courtneyrwilliams@students.abtech.edu>
2025-02-09 14:58:18 -0500

    Finished 4-2 Animals.

---
+++ animals.py
@@ -0,0 +1,6 @@
+big_cats = ['cheetah', 'jaguar', 'tiger']
+
+for big_cat in big_cats:
+    print(f'A {big_cat.title()} would be a facinating pet.')
+
+print('They are powerful hunters with sharp claws, keen senses, and distinctive coats.')
