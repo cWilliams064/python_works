125a93b9acb9e8a0c3356c650c1e994f27532c6a
Courtney Williams <courtneyrwilliams@students.abtech.edu>
2025-02-09 19:41:01 -0500

    Finished 4-15 Code Review.

---
+++ code-guideline-edited/my-own-list-edited.py
@@ -0,0 +1,5 @@
+car_brands = ['chevorlet', 'subaru', 'toyota', 'honda', 'cadilac']
+
+message = f'I would like to buy a {car_brands[0].title()} car.'
+
+print(message)
---
+++ code-guideline-edited/slices-edited.py
@@ -0,0 +1,72 @@
+guests = ['elizabeth short', 'ora murray', 'jonbenet ramsey']
+
+invitation = (
+    f'{guests[0].title()}, you are invited to a dinner reservation hosted by '
+    f'Courtney Williams.'
+)
+print(invitation)
+
+invitation = (
+    f'{guests[1].title()}, you are invited to a dinner reservation hosted by '
+    f'Courtney Williams.'
+)
+print(invitation)
+
+invitation = (
+    f'{guests[2].title()}, you are invited to a dinner reservation hosted by '
+    f'Courtney Williams.'
+)
+print(invitation)
+
+print('I found a biger dinner table for more guests.')
+
+guests.insert(0, 'marilyn monroe')
+guests.insert(1, 'joseph zarelli')
+guests.append('maria ridulph')
+
+guests.sort()
+
+invitation = (
+    f'{guests[0].title()}, you are invited to a dinner reservation hosted by '
+    f'Courtney Williams.'
+)
+print(invitation)
+
+invitation = (
+    f'{guests[1].title()}, you are invited to a dinner reservation hosted by '
+    f'Courtney Williams.'
+)
+print(invitation)
+
+invitation = (
+    f'{guests[2].title()}, you are invited to a dinner reservation hosted by '
+    f' Courtney Williams.'
+)
+print(invitation)
+
+invitation = (
+    f'{guests[3].title()}, you are invited to a dinner reservation hosted by '
+    f'Courtney Williams.'
+)
+print(invitation)
+
+invitation = (
+    f'{guests[4].title()}, you are invited to a dinner reservation hosted by '
+    f'Courtney Williams.'
+)
+print(invitation)
+
+invitation = (
+    f'{guests[5].title()}, you are invited to a dinner reservation hosted by '
+    f'Courtney Williams.'
+)
+print(invitation)
+
+print('The first three items in the list are:')
+print(guests[:3])
+
+print('Three items from the middle of the list are:')
+print(guests[1:4])
+
+print('The last three items in the list are:')
+print(guests[-3:])
