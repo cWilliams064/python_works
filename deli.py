96c4331a5a8a7836824f8458e762661cbe2e57e1
Courtney Williams <courtneyrwilliams@students.abtech.edu>
2025-02-18 04:09:57 -0500

    Finsihed 7-8 Deli.

---
+++ deli.py
@@ -0,0 +1,20 @@
+ordered_sandwiches = [
+    'classic clubhouse',
+    'veggie delight',
+    'smoky chicken',
+    'turkey avocado',
+    'spicy hummus crunch'
+]
+
+finished_sandwiches = []
+
+while ordered_sandwiches:
+    ordered_sandwich = ordered_sandwiches.pop()
+
+    print(f"I've made your {ordered_sandwich} Sandwich.")
+    finished_sandwiches.append(ordered_sandwich)
+
+print("\nThe following ordered sandwiches have been finished:")
+
+for fincished_sandwich in finished_sandwiches:
+    print(fincished_sandwich.title())
