82c393e205e97e6ae8402378b2f9fde4c2929920
Courtney Williams <courtneyrwilliams@students.abtech.edu>
2025-02-18 04:16:08 -0500

    Finished 7-8 Deli.

--- deli.py
+++ deli.py
@@ -11,10 +11,10 @@ finished_sandwiches = []
 while ordered_sandwiches:
     ordered_sandwich = ordered_sandwiches.pop()
 
-    print(f"I've made your {ordered_sandwich} Sandwich.")
+    print(f"I've made your {ordered_sandwich.title()} Sandwich.")
     finished_sandwiches.append(ordered_sandwich)
 
 print("\nThe following ordered sandwiches have been finished:")
 
 for finished_sandwich in finished_sandwiches:
-    print(fincished_sandwich.title())
+    print(finished_sandwich.title())
