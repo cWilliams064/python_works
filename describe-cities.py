5b484fcea178fc49ad7e34677bd4fbdcbad46ff8
Courtney Williams <courtneyrwilliams@students.abtech.edu>
2025-03-09 22:49:02 -0400

    Finished 8.5 Cities!

---
+++ describe-cities.py
@@ -0,0 +1,6 @@
+def describe_city(city, country='South Korea'):
+    print(f"{city.title()} is in {country.title()}")
+
+describe_city(city='seoul')
+describe_city(city='asheville', country='united states')
+describe_city(city='cairo', country='eqypt')
